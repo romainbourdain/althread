47f1cb3cd866711ba9e3f64656fbf2f1bab35802
Romain Bourdain <romain.bourdain93@gmail.com>
2024-08-19 10:20:30 +0200

    move interpreter to subfolder

---
+++ doc/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/romainbourdain/Documents/stage-icube/althread-with-pest/doc/node_modules/.pnpm/infima@0.2.0-alpha.44/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/romainbourdain/Documents/stage-icube/althread-with-pest/doc/node_modules/.pnpm/@docusaurus+theme-classic@3.5.2_@types+react@18.3.3_react-dom@18.3.1_react@18.3.1__react@18.3.1_typescript@5.5.4/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/romainbourdain/Documents/stage-icube/althread-with-pest/doc/node_modules/.pnpm/@docusaurus+theme-classic@3.5.2_@types+react@18.3.3_react-dom@18.3.1_react@18.3.1__react@18.3.1_typescript@5.5.4/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/romainbourdain/Documents/stage-icube/althread-with-pest/doc/src/css/custom.css"),
+];
